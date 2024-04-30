8dfbd2b0f249a907cf675b6f901c6b2ac2e12175
samuelrak <Samuel.rak@kosickaakademia.sk>
2024-04-30 15:09:58 +0200

    updated

--- minio/docker/back-up.sh
+++ minio/docker/back-up.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Dump MySQL database
 mysqldump -h mysql-host -u db-username -p db-name > /backup/wordpress_backup.sql
 
-# Set up MinIO alias and copy the backup file
 mc alias set myminio http://localhost:9090 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 mc cp /backup/wordpress_backup.sql myminio/backup/wordpress_backup.sql
