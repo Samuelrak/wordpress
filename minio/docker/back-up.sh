4d1f0af7cd839f08d4e60d36e6ec122d6f633701
samuelrak <Samuel.rak@kosickaakademia.sk>
2024-05-02 14:47:25 +0200

    workable minio

--- minio/docker/back-up.sh
+++ minio/docker/back-up.sh
@@ -6,8 +6,8 @@ MINIO_ACCESS_KEY="WuUESqfpsixGtKUm"
 #MINIO_SECRET_KEY="$MINIO_SECRET_KEY"
 MINIO_SECRET_KEY="YEK60CcdbySXj8RHETI84JTIWE1sWe"
 
-mysqldump -h mysql-host -u db-username -p db-name > /backup/wordpress_backup.sql
+tar -czf /backup/wordpress_backup_$(date +"%Y-%m-%d").tar.gz -C /var/www/html/ .
 
 mc alias set myminio "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-mc cp /backup/wordpress_backup.sql myminio/backup/wordpress_backup.sql
 
+mc cp /backup/wordpress_backup_$(date +"%Y-%m-%d").tar.gz myminio/backup/
